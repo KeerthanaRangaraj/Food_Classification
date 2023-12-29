2aa4d6caf0bb0bdac84ef24ea9ac1fa429cf3f28
KeerthanaRangaraj <keerthrangaraj@gmail.com>
2023-12-29 19:40:44 +0530

    made some changes in last line

--- application.py
+++ application.py
@@ -110,5 +110,4 @@ def index():
     return render_template('index.html', error=error, probabilities=probabilities, uploaded_image=uploaded_image)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    application.run(host='0.0.0.0', port=port, server='gunicorn')
+    application.run(debug=True)
